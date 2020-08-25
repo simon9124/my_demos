cc6a9bb5db9624a199b75ef2105c27160e819fd3
simon9124 <435776313@qq.com>
2020-08-25 14:39:13 +0800

    5.6 基本包装类型
    Signed-off-by: simon9124 <435776313@qq.com>

--- javascript高级程序设计/第5章 引用类型/5.6.基本包装类型.js
+++ javascript高级程序设计/第5章 引用类型/5.6.基本包装类型.js
@@ -69,12 +69,14 @@ console.log(numberValue instanceof Number) // false
 
 /* String 类型 */
 var stringObject = new String('hello world') // 调用String构造函数，并传入字符串
-
 console.log(stringObject.length) // "11"
+
+// 字符方法
 console.log(stringObject.charAt(1)) // "e"
 console.log(stringObject.charCodeAt(1)) // "101"
 console.log(stringObject[1]) // "e"
 
+// 字符串操作方法
 console.log(stringObject.concat(' wonderful')) // "hello world wonderful"
 console.log(stringObject.slice(3)) // "lo world"
 console.log(stringObject.substring(3)) // "lo world"
@@ -83,9 +85,103 @@ console.log(stringObject.slice(3, 7)) // "lo w"，第二个参数指定到哪里
 console.log(stringObject.substring(3, 7)) // "lo w"，第二个参数指定到哪里结束（不包含）
 console.log(stringObject.substr(3, 7)) // "lo worl"，第二个参数指定要返回的字符个数
 
-console.log(stringObject.slice(-3)) // "rld"
-console.log(stringObject.substring(-3)) // "hello world"
-console.log(stringObject.substr(-3)) // "rld"
-console.log(stringObject.slice(3, 7)) // "lo w"，第二个参数指定到哪里结束（不包含）
-console.log(stringObject.substring(3, 7)) // "lo w"，第二个参数指定到哪里结束（不包含）
-console.log(stringObject.substr(3, 7)) // "lo worl"，第二个参数指定要返回的字符个数
+console.log(stringObject.slice(-3)) // "rld"，-3转换为11-3=8
+console.log(stringObject.substring(-3)) // "hello world"，-3转换为0
+console.log(stringObject.substr(-3)) // "rld"，-3转换为11-3=8
+console.log(stringObject.slice(3, -4)) // "lo w"，-4转换为11-3=7
+console.log(stringObject.substring(3, -4)) // "lo w"，-4转换为0，substring(3,0)再转换为substring(0,3)
+console.log(stringObject.substr(3, -4)) // ""，-4转换为0，返回包含零个字符的字符串
+
+console.log(stringObject.indexOf('o')) // 4
+console.log(stringObject.lastIndexOf('o')) // 7
+console.log(stringObject.indexOf('o', 6)) // 7，第二个参数表示从哪个位置开始搜索
+console.log(stringObject.lastIndexOf('o', 6)) // 4，第二个参数表示从哪个位置开始搜索
+
+// 字符串位置方法
+var loremString =
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+var position = new Array()
+var pos = loremString.indexOf('o')
+while (pos > -1) {
+  position.push(pos)
+  pos = loremString.indexOf('o', pos + 1)
+}
+console.log(position) // 所有包含o的字符位置集合
+
+// trim()方法
+var stringValue = '  hello world  '
+var trimStringValue = stringValue.trim()
+console.log(stringValue) // "  hello world  "
+console.log(trimStringValue) // "hello world"
+
+// 字符串大小写转换方法
+console.log(stringObject.toUpperCase()) // "HELLO WORLD"
+console.log(stringObject.toLocaleUpperCase()) // "HELLO WORLD"
+console.log(stringObject.toLowerCase()) // "hello world"
+console.log(stringObject.toLocaleLowerCase()) // "hello world"
+
+// 字符串模式匹配方法
+var text = 'cat, bat, sat, fat'
+var pattern = /.at/
+
+var matches = text.match(pattern)
+console.log(matches) // [ 'cat',index: 0,input: 'cat, bat, sat, fat',groups: undefined ]，match方法返回一个数组，第一项与整个模式匹配的字符串，后面的项保存着与正则表达式捕获匹配的字符串
+console.log(matches.index) // 0
+console.log(matches[0]) // "cat"
+console.log(pattern.lastIndex) // 0
+
+var pos = text.search(/at/)
+console.log(pos) // 1，记录"at"首次在字符串出现的位置
+
+var result = text.replace('at', 'ond')
+console.log(result) // "cond, bat, sat, fat"，第一个参数是字符串所以只替换第一个匹配的
+result = text.replace(/at/g, 'ond')
+console.log(result) // "cond, bond, sond, fond"，第一个参数是全局正则所以替换全部的
+result = text.replace(/(.at)/g, 'word($1)')
+console.log(result) // "word(cat), word(bat), word(sat), word(fat)"，$1表示第一个匹配的参数
+
+function htmlEscape(text) {
+  return text.replace(/[<>"&]/g, function (match, pos, originalText) {
+    switch (match) {
+      case '<':
+        return '&lt;'
+        break
+      case '>':
+        return '&gt;'
+        break
+      case '&':
+        return '&amp;'
+        break
+      case '"':
+        return '&quot;'
+        break
+    }
+  })
+}
+console.log(htmlEscape('<p class="greeting">Hello world!</p>')) // "&lt;p class=&quot;greeting&quot;&gt;Hello world!&lt;/p&gt;"
+
+console.log(text.split(',')) // [ 'cat', ' bat', ' sat', ' fat' ]
+console.log(text.split(',', 2)) // [ 'cat', ' bat' ]，第二个参数指定数组大小
+console.log(text.split(/[^\,]+/)) // [ '', ',', ',', ',', '' ]，通过正则获取包含都好字符的数组，正则指定分隔符出现在了字符串开头和末尾，因此首位是空字符串
+
+// localeCompare()方法
+var stringCompare = 'yellow'
+console.log(stringCompare.localeCompare('brick')) // 1，brick字母表在yellow之前
+console.log(stringCompare.localeCompare('yellow')) // 0，yellow字母表与yellow相同
+console.log(stringCompare.localeCompare('zoo')) // -1，zoo字母表在yellow之后
+
+function determineOrder(value) {
+  var result = stringCompare.localeCompare(value)
+  result < 0 &&
+    console.log(`The string 'yellow' comes before the string ${value}.`)
+  result > 0 &&
+    console.log(`The string 'yellow' comes after the string ${value}.`)
+  result === 0 &&
+    console.log(`The string 'yellow' is equal to the string ${value}.`)
+}
+determineOrder('brick') // "The string 'yellow' comes after the string brick."
+determineOrder('yellow') // "The string 'yellow' is equal to the string zoo."
+determineOrder('zoo') // "The string 'yellow' comes before the string zoo."
+
+// fromCharCode()方法
+console.log(String.fromCharCode(104, 101, 108, 108, 111)) // "hello"
