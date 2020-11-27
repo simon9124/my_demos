091675480a18862b8bf30f88611cc724632c1aa8
simon9124 <435776313@qq.com>
2020-11-27 18:22:35 +0800

    临时
    Signed-off-by: simon9124 <435776313@qq.com>

--- javascript高级程序设计（第三版）/第6章 面向对象的程序设计/6.2.创建对象.js
+++ javascript高级程序设计（第三版）/第6章 面向对象的程序设计/6.2.创建对象.js
@@ -9,9 +9,25 @@ function createPerson(name, age, job) {
   }
   return o
 }
-var person1 = createPerson('Nicholas', 29, 'Engineer')
+var person1 = createPerson('Nicholas', 29, 'Software Engineer')
 var person2 = createPerson('Greg', 27, 'Doctor')
 console.log(person1)
 console.log(person2)
 
 /* 构造函数模式 */
+function Person(name, age, job) {
+  this.name = name
+  this.age = age
+  this.job = job
+  this.sayName = function () {
+    console.log(this.name)
+  }
+}
+var person1 = new Person('Nicholas', 29, 'Software Engineer')
+var person2 = new Person('Greg', 27, 'Doctor')
+console.log(person1.constructor === Person) // true，constructor 属性指向构造函数
+console.log(person2.constructor === Person) // true，constructor 属性指向构造函数
+console.log(person1 instanceof Object) // true，是 Object 的实例
+console.log(person1 instanceof Person) // true，也是 Person 的实例
+console.log(person2 instanceof Object) // true，是 Object 的实例
+console.log(person2 instanceof Person) // true，也是 Person 的实例
