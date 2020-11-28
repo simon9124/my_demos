6c655c2a55e1f3b0564f3380c37b7533f6968b5e
simon9124 <435776313@qq.com>
2020-11-28 17:36:34 +0800

    6.2.创建对象
    Signed-off-by: simon9124 <435776313@qq.com>

--- javascript高级程序设计（第三版）/第6章 面向对象的程序设计/6.2.创建对象.js
+++ javascript高级程序设计（第三版）/第6章 面向对象的程序设计/6.2.创建对象.js
@@ -31,3 +31,52 @@ console.log(person1 instanceof Object) // true，是 Object 的实例
 console.log(person1 instanceof Person) // true，也是 Person 的实例
 console.log(person2 instanceof Object) // true，是 Object 的实例
 console.log(person2 instanceof Person) // true，也是 Person 的实例
+
+// 构造函数vs普通函数
+var person3 = new Person('Nicholas', 29, 'Software Engineer') // 用构造函数创建对象
+person3.sayName() // 'Nicholas'
+Person('Greg', 27, 'Doctor') // 'Greg'，不使用new操作符，直接调用
+global.sayName() // 直接调用函数时，this指向Global对象（浏览器中指向window对象）
+var o = new Object() // 新对象o
+var p = new Object() // 新对象p
+Person.call(o, 'Kristen', 25, 'Nurse') // 扩充作用域，在对象o中调用Person()函数，call()分别传入每个参数
+Person.apply(p, ['Kristen', 25, 'Nurse']) // 扩充作用域，在对象p中调用Person()函数，apply()传入参数数组
+o.sayName() // 'Kristen'
+p.sayName() // 'Kristen'
+
+// 构造函数的缺点
+function Person2(name, age, job) {
+  this.name = name
+  this.age = age
+  this.job = job
+  this.sayName = new Function(console.log(this.name)) // 与声明函数逻辑等价，每创建一个对象就要创建一个Function实例
+}
+console.log(person1.sayName === person2.sayName) // false，新对象的2个方法的作用域链和标识符解析不同
+
+// 避免多次创建 Function 实例
+function Person3(name, age, job) {
+  this.name = name
+  this.age = age
+  this.job = job
+  this.sayName = sayName
+}
+function sayName() {
+  // 将sayName设置成全局函数
+  console.log(this.name)
+}
+var person4 = new Person('Nicholas', 29, 'Software Engineer')
+
+/* 原型模式 */
+function PersonPrototype() {}
+PersonPrototype.prototype.name = 'Nicholas' // 为PersonPrototype的原型对象添加属性
+PersonPrototype.prototype.age = 29 // 为PersonPrototype的原型对象添加属性
+PersonPrototype.prototype.job = 'Software Engineer' // 为PersonPrototype的原型对象添加属性
+PersonPrototype.prototype.sayName = function () {
+  // 为PersonPrototype的原型对象添加方法
+  console.log(this.name)
+}
+var person5 = new PersonPrototype()
+var person6 = new PersonPrototype()
+person5.sayName() // 'Nicholas'
+person6.sayName() // 'Nicholas'
+console.log(person5.sayName === person6.sayName) // true，prototype上创建的属性和方法，由新对象的所有实例共享
