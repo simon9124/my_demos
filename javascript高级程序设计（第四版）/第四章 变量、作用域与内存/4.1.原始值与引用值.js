ed4e5aff52daaaa04b0e64f7f538c84eb9c7a506
simon9124 <435776313@qq.com>
2021-02-13 00:03:36 +0800

    4.1.原始值与引用值
    Signed-off-by: simon9124 <435776313@qq.com>

--- javascript高级程序设计（第四版）/第四章 变量、作用域与内存/4.1.原始值与引用值.js
+++ javascript高级程序设计（第四版）/第四章 变量、作用域与内存/4.1.原始值与引用值.js
@@ -17,7 +17,46 @@ console.log(typeof name1) // string，原始类型
 console.log(typeof name2) // object，引用类型
 
 /* 复制值 */
+let num1Prim = 5
+let num2Prim = num1Prim
+console.log(num2Prim) // 5，原始值，复制的值是副本
+num2Prim = 6 // 副本发生改变
+console.log(num2Prim) // 6
+console.log(num1Prim) // 5，被复制的值无变化
+
+let obj1Refer = new Object()
+let obj2Refer = obj1Refer // 引用值，复制的值是指针
+obj2Refer.name = 'Nicholas' // 一个对象发生改变
+console.log(obj2Refer.name) // 'Nicholas'
+console.log(obj1Refer.name) // 'Nicholas'，影响另一个对象
+delete obj1Refer.name // 一个对象发生改变
+console.log(obj1Refer.name) // undefined
+console.log(obj2Refer.name) // undefined，影响另一个对象
 
 /* 传递参数 */
+let count = 10 // 函数外，原始值作为参数
+function addTen(num) {
+  num += 10 // 函数内，参数的值发生改变
+  return num
+}
+let result = addTen(count)
+console.log(result) // 30
+console.log(count) // 20，未受影响
+
+let person = new Object() // 函数外，引用值作为参数
+function setName(obj) {
+  obj.name = 'Nicholas' // 函数内，obj和外部参数person指向同一个对象，并改变了这个对象的属性
+}
+setName(person)
+console.log(person.name) // 'Nicholas'，受影响
+
+let person2 = new Object()
+function setName2(obj) {
+  obj.name = 'Nicholas' // 改变参数的属性，参数受影响
+  obj = new Object() // 重写参数，参数不受该影响
+  obj.name = 'Greg'
+}
+setName2(person2)
+console.log(person2.name) // 'Nicholas'
 
 /* 确定类型 */
