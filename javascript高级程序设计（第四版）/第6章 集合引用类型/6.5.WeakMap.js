c94d43efeae51dea03cd2ace1b7aba8be297d871
simon9124 <435776313@qq.com>
2021-03-25 17:55:44 +0800

    6.5.WeakMap
    Signed-off-by: simon9124 <435776313@qq.com>

--- javascript高级程序设计（第四版）/第6章 集合引用类型/6.5.WeakMap.js
+++ javascript高级程序设计（第四版）/第6章 集合引用类型/6.5.WeakMap.js
@@ -65,10 +65,82 @@ const container = { key: {} } // container对象维护着wm7中键的引用
 wm7.set(container.key, 'val') // 键和值均不会成为垃圾回收的目标
 container.key = null // 切断键对象的引用，键和值才会被垃圾回收
 
-/* 不可迭代键 */
-
 /* 使用弱映射 */
 
 // 私有变量
+const wm8 = new WeakMap()
+
+class User {
+  constructor(id) {
+    this.idProperty = Symbol('id') // 创建符号
+    this.setId(id)
+  }
+  setId(id) {
+    this.setPrivate(this.idProperty, id)
+  }
+  setPrivate(property, value) {
+    const privateMembers = wm8.get(this) || {}
+    privateMembers[property] = value // { idProperty: Symbol(id): id }
+    wm8.set(this, privateMembers)
+  }
+  getId() {
+    return this.getPrivate(this.idProperty)
+  }
+  getPrivate(property) {
+    return wm8.get(this)[property] // 获取到id
+  }
+}
+const user = new User(123)
+console.log(user) // User { idProperty: Symbol(id) }
+console.log(wm8) // WeakMap { User => { idProperty: Symbol(id) } }
+console.log(user.getId()) // 123
+
+user.setId(456)
+console.log(user.getId()) // 456
+
+console.log(wm8.get(user)) // { [Symbol(id)]: 456 }
+console.log(wm8.get(user)[user.idProperty]) // 456，外部变量拿到对象实例的引用和弱映射，取得私有变量
+
+const UserClosure = (() => {
+  // 用闭包把WeakMap包起来
+  const wm9 = new WeakMap()
+
+  class User {
+    constructor(id) {
+      this.idProperty = Symbol('id') // 创建符号
+      this.setId(id)
+    }
+    setId(id) {
+      this.setPrivate(this.idProperty, id)
+    }
+    setPrivate(property, value) {
+      const privateMembers = wm9.get(this) || {}
+      privateMembers[property] = value // { idProperty: Symbol(id): id }
+      wm9.set(this, privateMembers)
+    }
+    getId() {
+      return this.getPrivate(this.idProperty)
+    }
+    getPrivate(property) {
+      return wm9.get(this)[property] // 获取到id
+    }
+  }
+  return User
+})()
+const user2 = new UserClosure(123)
+console.log(user2) // User { idProperty: Symbol(id) }
+console.log(user2.getId()) // 123
+
+user2.setId(456)
+console.log(user2.getId()) // 456
+
+// console.log(wm9) // ReferenceError: wm9 is not defined，拿不到弱映射，无法获取弱映射中对应的值，成功设置私有变量
 
 // DOM 节点元数据
+const m = new Map()
+const loginButton = document.querySelector('#login')
+m.set(loginButton, { disabled: true }) // 给节点关联元数据，保存在映射中，即使DOM被删除映射仍然存在
+
+const wm10 = new WeakMap()
+const loginButtin2 = document.querySelector('#login')
+wm10.set(loginButton, { disabled: true }) // 给节点关联元数据，保存在弱映射中，DOM被删除（若没有其他地方引用loginButton）弱映射被回收
