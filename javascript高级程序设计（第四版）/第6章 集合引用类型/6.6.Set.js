19f004202885701ad8326e23e7c9494909a994ee
simon9124 <435776313@qq.com>
2021-04-01 16:55:36 +0800

    6.6.Set
    Signed-off-by: simon9124 <435776313@qq.com>

--- javascript高级程序设计（第四版）/第6章 集合引用类型/6.6.Set.js
+++ javascript高级程序设计（第四版）/第6章 集合引用类型/6.6.Set.js
@@ -77,4 +77,68 @@ console.log(s7) // Set(0) {}
 
 /* 顺序与迭代 */
 
+// 迭代器
+const s8 = new Set(['val1', 'val2', 'val3'])
+console.log(s8.values === s8[Symbol.iterator]) // true
+console.log(s8.keys === s8[Symbol.iterator]) // true
+
+for (let value of s8.values()) {
+  console.log(value)
+  /* 
+    val1
+    val2
+    val3
+  */
+}
+for (let value of s8[Symbol.iterator]()) {
+  console.log(value)
+  /* 
+    val1
+    val2
+    val3
+  */
+}
+
+// 扩展操作
+console.log([...s8]) // [ 'val1', 'val2', 'val3' ]
+console.log(...s8) // 'val1', 'val2', 'val3'
+
+// 回调方式
+s8.forEach((val, dupVal) => {
+  console.log(`${val}->${dupVal}`)
+  /* 
+    val1->val1
+    val2->val2
+    val3->val3
+  */
+})
+
+// 遍历时修改键或值
+const s9 = new Set(['val1', 'val2']) // 字符串原始值作为元素
+
+for (let value of s9.values()) {
+  value = 'newVal' // 修改元素
+  console.log(value) // 'newVal'
+  console.log(s9.has('newVal')) // false，集合内部的引用无法修改
+  console.log(s9.has('val1')) // true
+}
+console.log(s9) // Set(2) { 'val1', 'val2' }，集合不受影响
+
+const valObj = { id: 1 }
+const s10 = new Set([valObj]) // 引用值作为元素
+
+for (let value of s10.values()) {
+  value.id = 2 // 修改元素的属性
+  console.log(value) // { id: 2 }
+  console.log(s10.has(valObj)) // true，集合内部的引用未改变，对象在集合内部仍然引用相同的值
+}
+console.log(s10) // Set(1) { { id: 2 } }，集合受影响
+
+for (let value of s10.values()) {
+  value = { id: 3 } // 重写元素
+  console.log(value) // { id: 3 }
+  console.log(s10.has(valObj)) // true
+}
+console.log(s10) // Set(1) { { id: 2 } }，集合不受影响
+
 /* 定义正式集合操作 */
